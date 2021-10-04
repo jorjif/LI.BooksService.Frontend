9f2ac30005f5ea9690f6b9acc1e447e44f85a6cc
jorji <panenko01@mail.ru>
2021-10-04 23:53:15 +0300

    ss-46 адаптировал каркас страницы под макет

--- src/components/navigation/menu/nav.tsx
+++ src/components/navigation/menu/nav.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import "./nav.scss";
 import NavComponent from "./menuElement";
-import { ReactComponent as ShareBook } from "./icons/giveBook.svg";
-import { ReactComponent as GetBook } from "./icons/getBook.svg";
 import { ReactComponent as Trades } from "./icons/currTrades.svg";
-import { ReactComponent as Archive } from "./icons/archive.svg";
-import { ReactComponent as Rating } from "./icons/rating.svg";
 import { ReactComponent as Admin } from "./icons/administration.svg";
+import { ReactComponent as Home } from "./icons/Home.svg";
+import { ReactComponent as Notifications } from "./icons/notifications.svg";
+import { ReactComponent as Trade } from "./icons/trade.svg";
+import { ReactComponent as User } from "./icons/user.svg";
+
 const Navbar: React.FC = () => {
   return (
     <nav className="page_frame_nav">
       <ul className="page_frame_nav_list">
-        <NavComponent icon={<ShareBook />}>Хочу поделиться</NavComponent>
-        <NavComponent icon={<GetBook />}>Хочу получить</NavComponent>
-        <NavComponent icon={<Trades />}>Текущие обмены</NavComponent>
-        <NavComponent icon={<Archive />}>Архив</NavComponent>
-        <NavComponent icon={<Rating />}>Рейтинг</NavComponent>
+        <NavComponent icon={<Home />}>Главная</NavComponent>
+        <NavComponent icon={<Trade />}>Начать обмен</NavComponent>
+        <NavComponent icon={<Trades />}>Мои обмены</NavComponent>
+        <NavComponent icon={<User />}>Личные данные</NavComponent>
+        <NavComponent icon={<Notifications />}>Уведомления</NavComponent>
         <NavComponent icon={<Admin />}>Связаться с администрацией</NavComponent>
       </ul>
     </nav>

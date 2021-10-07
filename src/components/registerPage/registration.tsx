4f9c9af7033de7c2b633fffa934fad5d9c33c74b
jorji <panenko01@mail.ru>
2021-10-07 20:15:28 +0300

    ss-26 forgot register button. Added it

--- src/components/registerPage/registration.tsx
+++ src/components/registerPage/registration.tsx
@@ -1,4 +1,4 @@
-import { Paper } from "@material-ui/core";
+import { Button, Paper } from "@material-ui/core";
 import React from "react";
 import "./registration.scss";
 import { ReactComponent as Kid } from "../mainPage/kid.svg";
@@ -13,12 +13,24 @@ const Registration: React.FC = () => {
     alignItems: "flex-start",
   };
   return (
-    <Paper elevation={3} className="page_mainPage_paper">
-      <Kid className="page_mainPage_icon" />
+    <Paper elevation={3} className="page_registerPage_paper">
+      <Kid className="page_registerPage_icon" />
       <Box sx={boxStyles}>
-        <h1 className="page_mainPage_h1">Книгомен</h1>
-        <h2 className="page_mainPage_h2">Регистрация</h2>
+        <h1 className="page_registerPage_h1">Книгомен</h1>
+        <h2 className="page_regiserPage_h2">Регистрация</h2>
         <RegisterInput />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            width: "100%",
+            m: "8px 0",
+          }}
+        >
+          <Button variant="contained" className="page_registerPage_button">
+            Зарегистрироваться
+          </Button>
+        </Box>
       </Box>
     </Paper>
   );

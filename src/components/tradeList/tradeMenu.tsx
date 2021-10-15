bddfe112b4d52ffdd07d0b2d781bcee6db9ff55d
jorji <panenko01@mail.ru>
2021-10-15 17:55:40 +0300

    ss-80 added pagination

--- src/components/tradeList/tradeMenu.tsx
+++ src/components/tradeList/tradeMenu.tsx
@@ -1,5 +1,5 @@
-import { Box, Tab, Tabs } from "@material-ui/core";
-import React from "react";
+import { Box, Pagination, Tab, Tabs } from "@material-ui/core";
+import React, { useState } from "react";
 import TextInput from "../customInputs/TextInput";
 
 /*function tabProps(index: number) {
@@ -13,24 +13,34 @@ interface IProps {
 }
 
 const TradeMenu: React.FC<IProps> = ({ children }) => {
+  const [filter, setFilter] = useState(0);
+  const handleFilterChange = (event: React.SyntheticEvent, newFilter: number) => {
+    setFilter(newFilter);
+  };
   return (
     <Box sx={{ display: "flex", flexDirection: "column" }}>
       <h1 className="page_tradePage_h1">Мои обмены</h1>
       <Box
         sx={{
           display: "flex",
-          marginBottom: "1rem",
+          marginBottom: "2rem",
           justifyContent: "space-between",
+          alignItems: "flex-start",
         }}
       >
         <TextInput size="small" />
-        <Tabs>
+        <Tabs value={filter} onChange={handleFilterChange}>
           <Tab label="Все" />
           <Tab label="Идет обмен" />
           <Tab label="Завершенные" />
         </Tabs>
       </Box>
       {children}
+      <Pagination
+        shape="rounded"
+        count={10}
+        sx={{ alignSelf: "center", marginTop: "2rem" }}
+      />
     </Box>
   );
 };
